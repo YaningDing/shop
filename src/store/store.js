846717cd58b173ac1a1c0429c4af587d6813947b
ynd <ding.yani@northeastern.edu>
2022-06-03 18:53:57 -0700

    first

---
+++ src/store/store.js
@@ -0,0 +1,47 @@
+import { compose, createStore, applyMiddleware } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+//import thunk from 'redux-thunk'
+// import logger from 'redux-logger';
+import { rootSaga } from './root-saga';
+import createSagaMiddleware from '@redux-saga/core';
+
+import { rootReducer } from './root-reducer';
+
+import logger from 'redux-logger';
+
+
+const sagaMiddleware = createSagaMiddleware();
+const middleWares = [
+  process.env.NODE_ENV === 'development' && logger,
+  sagaMiddleware,
+].filter(Boolean);
+
+
+
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart'],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const composeEnhancer =
+  (process.env.NODE_ENV !== 'production' &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+
+const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
+
+export const store = createStore(
+  persistedReducer,
+  undefined,
+  composedEnhancers
+);
+
+sagaMiddleware.run(rootSaga);
+
+export const persistor = persistStore(store);
